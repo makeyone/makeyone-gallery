92a60b9ad061fe05199e70116e0c347f726d155c
Kim LeeJun <hwanam1111@naver.com>
2023-12-18 12:15:00 +0900

    ✨ feat: footer 추가 및 font weight 300 추가

---
+++ src/app/_components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Link from 'next/link';
+
+import { bindClassNames } from '@/libs/bind-class-name';
+
+import styles from './Footer.module.css';
+
+const cx = bindClassNames(styles);
+
+export default function Footer() {
+  return (
+    <footer className={cx('footer')}>
+      <p className={cx('copyright')}>&copy; Makeyone(Juniya) all rights reserved.</p>
+      <ul className={cx('policyList')}>
+        <li className={cx('policyItem')}>
+          <Link href="/terms/service">서비스이용약관</Link>
+        </li>
+        <li className={cx('policyItem')}>
+          <Link href="/terms/privacy">개인정보처리방침</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 
 import { Roboto } from 'next/font/google';
 
+import Footer from '@/app/_components/Footer';
 import Header from '@/app/_components/Header';
 
 import CoreProvider from '@/providers/CoreProvider';
@@ -19,7 +20,7 @@ const roboto = Roboto({
   subsets: ['latin'],
   style: ['normal', 'italic'],
   display: 'swap',
-  weight: ['100', '400', '500', '700'],
+  weight: ['100', '300', '400', '500', '700'],
 });
 
 export default async function RootLayout({ children }: Props) {
@@ -29,6 +30,7 @@ export default async function RootLayout({ children }: Props) {
         <CoreProvider>
           <Header />
           <main className="main">{children}</main>
+          <Footer />
         </CoreProvider>
       </body>
     </html>
