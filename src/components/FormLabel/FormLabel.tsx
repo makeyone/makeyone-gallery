131ac841c778b202b034f8772041f45696483927
Kim LeeJun <hwanam1111@naver.com>
2024-01-25 17:28:39 +0900

    ✨ feat: 폼 라벨, 라디오 그룹

---
+++ src/components/FormLabel/FormLabel.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { bindClassNames } from '@/libs/bind-class-name';
+
+import styles from './FormLabel.module.css';
+
+const cx = bindClassNames(styles);
+
+type Props = {
+  label: string;
+  inputId?: string;
+};
+
+export default function FormLabel({ inputId = '', label }: Props) {
+  return (
+    <div className={cx('root')}>
+      <label className={cx('label')} htmlFor={inputId}>
+        {label}
+      </label>
+    </div>
+  );
+}
---
+++ src/components/FormRadioGroupWithLabel/FormRadioGroupWithLabel.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import FormLabel from '@/components/FormLabel';
+
+import { bindClassNames } from '@/libs/bind-class-name';
+
+import styles from './FormRadioGroupWithLabel.module.css';
+
+const cx = bindClassNames(styles);
+
+export type FormRadioGroupItemType = {
+  id: string;
+  label: string;
+  value: string;
+};
+
+type Props = {
+  label: string;
+  register: UseFormRegisterReturn;
+  name: string;
+  items: FormRadioGroupItemType[];
+  required?: boolean;
+  align?: 'Col' | 'Row';
+};
+
+export default function FormRadioGroupWithLabel({ label, register, items, required = false, align = 'Col' }: Props) {
+  return (
+    <div className={cx('root')}>
+      <FormLabel label={label} />
+      <div className={cx('radioGroup', align.toLowerCase())}>
+        {items.map((item) => (
+          <div key={item.id}>
+            <input
+              type="radio"
+              className={cx('hiddenInput')}
+              id={item.id}
+              aria-label={item.label}
+              value={item.value}
+              required={required}
+              hidden
+              {...register}
+            />
+            <label htmlFor={item.id} className={cx('radio')}>
+              {item.value && <i className={cx('checkIcon')} />}
+              <span className={cx('label')}>{item.label}</span>
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
