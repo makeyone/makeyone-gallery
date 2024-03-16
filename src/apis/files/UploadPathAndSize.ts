c9110d549e95e1dd7948c549d29d31469ba939d8
Kim LeeJun <hwanam1111@naver.com>
2024-03-17 03:07:05 +0900

    🔨 fix: 이미지 업로드 최대 용량 사이즈 수정

--- src/apis/files/UploadPathAndSize.ts
+++ src/apis/files/UploadPathAndSize.ts
@@ -2,11 +2,11 @@ const UploadPathAndSize = {
   posts: {
     mainAndListImages: {
       uploadPath: `uploads/${process.env.NODE_ENV}/posts/images/main_and_list`,
-      maxSize: 10,
+      maxSize: 30,
     },
     contentImages: {
       uploadPath: `uploads/${process.env.NODE_ENV}/posts/images/content`,
-      maxSize: 1,
+      maxSize: 30,
     },
   },
 };
