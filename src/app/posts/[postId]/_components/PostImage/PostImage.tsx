abd15534c1c1e3af6ba25c3d43f0d7a15f359ba9
Kim LeeJun <hwanam1111@naver.com>
2024-03-17 18:07:44 +0900

    ✨ feat: 이미지 갤러리 형식으로 사진 렌더링

---
+++ src/app/posts/[postId]/_components/PostImage/PostImage.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
+
+import { useParams } from 'next/navigation';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { getPostById } from '@/apis/posts/actions/GetPostById';
+import { postsQueryKeys } from '@/apis/posts/posts.query-keys';
+
+import { bindClassNames } from '@/libs/bind-class-name';
+
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+import '@/styles/image-gallery.css';
+
+import styles from './PostImage.module.css';
+
+const cx = bindClassNames(styles);
+
+type Props = {};
+
+export default function PostImage({}: Props) {
+  const params = useParams();
+  const postId = parseInt(params.postId as string, 10);
+  const { data } = useQuery({
+    queryKey: postsQueryKeys.byId(postId),
+    queryFn: () => getPostById({ postId }),
+  });
+  const post = data?.post;
+
+  const postImages = post?.postImages.map((image) => ({
+    original: image.imageUrl,
+    thumbnail: image.imageUrl,
+    loading: 'lazy',
+    originalHeight: 600,
+    thumbnailLoading: 'lazy',
+    thumbnailHeight: 90,
+    originalClass: 'gallery_original_img',
+    thumbnailClassClass: 'gallery_thumbnail_img',
+    bulletClass: '',
+  })) as ReactImageGalleryItem[];
+
+  return (
+    <div className={cx('root')}>
+      <ImageGallery
+        items={postImages}
+        showIndex
+        infinite
+        lazyLoad
+        showBullets
+        showPlayButton={false}
+        showThumbnails
+        thumbnailPosition="right"
+        renderCustomControls={() => <div className="blur_bg" />}
+      />
+    </div>
+  );
+}
---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+
+import { getPostById } from '@/apis/posts/actions/GetPostById';
+import { GetPostByIdError, GetPostByIdOutput } from '@/apis/posts/dtos/GetPostById.dto';
+import { postsQueryKeys } from '@/apis/posts/posts.query-keys';
+
+import PostImage from '@/app/posts/[postId]/_components/PostImage';
+
+import Footer from '@/components/Layout/Footer';
+import Header from '@/components/Layout/Header';
+
+import { bindClassNames } from '@/libs/bind-class-name';
+import getAxiosErrorMessage from '@/libs/get-axios-error-message';
+
+import styles from './page.module.css';
+
+const cx = bindClassNames(styles);
+
+type Props = {
+  params: {
+    postId: string;
+  };
+};
+
+async function getPostData(postId: number) {
+  try {
+    const res = await getPostById({ postId });
+    return res.post;
+  } catch (err: any) {
+    const errorMessage = getAxiosErrorMessage<GetPostByIdOutput, typeof GetPostByIdError>(err);
+    if (errorMessage === 'POST_NOT_FOUND') {
+      return redirect('/not-found');
+    }
+    throw new Error('SERVER_ERROR');
+  }
+}
+
+export async function generateMetadata({ params: { postId } }: Props): Promise<Metadata> {
+  const post = await getPostData(parseInt(postId, 10));
+
+  return {
+    title: `${post?.postTitle} - 메이키원 갤러리`,
+  };
+}
+
+export default async function PostPage({ params: { postId } }: Props) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: postsQueryKeys.byId(parseInt(postId as string, 10)),
+    queryFn: () => getPostById({ postId: parseInt(postId as string, 10) }),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <HydrationBoundary state={dehydratedState}>
+          <div className={cx('root')}>
+            <PostImage />
+          </div>
+        </HydrationBoundary>
+      </main>
+      <Footer />
+    </>
+  );
+}
