68e012c67dcfaba32d1aece131fae7062892d4bf
Kim LeeJun <hwanam1111@naver.com>
2025-03-09 23:09:36 +0900

    ✨ feat: root error page 분기 처리

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,45 @@
 'use client';
 
-export default function Error() {
+import { useRouter } from 'next/navigation';
+
+import { ApiResponse } from '@/api/support/response/ApiResponse';
+
+type Props = {
+  error: ApiResponse<any, any>;
+};
+
+export default function RootError({ error: { error } }: Props) {
+  const { push } = useRouter();
+
+  const errorCode = error?.code;
+
+  switch (errorCode) {
+    case 'NOT_LOGGED_IN':
+      push('/users/login');
+      break;
+    case 'NON_EXISTENT_USER':
+      // TODO: jwt 날리기
+      push('/users/login');
+      break;
+    case 'NOT_ACTIVED_USER':
+      // TODO: jwt 날리기
+      push('/users/login');
+      break;
+    case 'DO_NOT_HAVE_PERMISSION':
+      // TODO: jwt 날리기
+      push('/users/login');
+      break;
+    case 'INVALID_JWT_ACCESS_TOKEN':
+      // TODO: jwt 날리기
+      push('/users/login');
+      break;
+    default:
+      break;
+  }
+
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>에러가 발생하였습니다.</h2>
     </div>
   );
 }
