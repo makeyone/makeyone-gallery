f25e2398032aae92a799e0ebdf017c9d02814651
Kim LeeJun <hwanam1111@naver.com>
2025-03-10 15:09:25 +0900

    📝 docs: 기본 배열들의 keyboard json 파일 수정

--- public/customDefinitions/index.ts
+++ public/customDefinitions/index.ts
@@ -1,11 +1,15 @@
-import dc67 from './dc67_via.json';
-import default40 from './default40.json';
-import em8 from './em8.json';
-import linkHotswap from './LINK_hotswap.json';
-import neo70 from './neo70_via_v1_03_20231130.json';
+import default100 from './default_100.json';
+import default40 from './default_40.json';
+import default60 from './default_60.json';
+import default65 from './default_65.json';
+import default70 from './default_70.json';
+import default75 from './default_75.json';
+import defaultFull from './default_full.json';
+import defaultHHKB from './default_hhkb.json';
+import defaultTKL from './default_tkl.json';
 
 const etc = {
   name: '아래의 레이아웃중 해당하는 항목이 없음',
 };
 
-export default [etc, default40, linkHotswap, em8, dc67, neo70];
+export default [etc, defaultFull, default100, defaultTKL, default75, default70, default65, default60, defaultHHKB, default40];
