80b7b13c684d5bcee0499e1644d2c82b301365e4
Kim LeeJun <hwanam1111@naver.com>
2025-04-22 14:52:59 +0900

    ✨ feat: vh height area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
+  useEffect(() => {
+    const documentHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    documentHeight();
+    window.addEventListener('resize', documentHeight);
+    return () => {
+      window.removeEventListener('resize', documentHeight);
+    };
+  }, []);
+
   return children;
 }
