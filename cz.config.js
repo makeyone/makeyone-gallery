98c39c42092f92b5b722048713ddda9b8e0ce0d4
Kim LeeJun <hwanam1111@naver.com>
2025-04-23 17:10:26 +0900

    docs: update github commit convention

--- cz.config.js
+++ cz.config.js
@@ -1,17 +1,25 @@
+const chalk = require('chalk');
+
+const padLabel = (label, length = 10) => {
+  const padLength = length - label.length;
+  return label + ' '.repeat(padLength > 0 ? padLength : 0);
+};
+
 module.exports = {
   types: [
-    { value: '✨ feat ', name: '✨ feat:\t새로운 기능 추가' },
-    { value: '🔨 fix ', name: '🔨 fix:\t기능 및 버그 수정' },
-    { value: '💎 design ', name: '💎 design:\t사용자 UI 수정' },
-    { value: '🎨 style ', name: '🎨 style:\t코드 포맷, 세미콜론 누락 등 코드변경이 없는 경우' },
-    { value: '♻️ refactor ', name: '♻️ refactor:\t코드 리펙토링' },
-    { value: '🧪 test ', name: '🧪 test:\t테스트코드 추가 및 수정' },
-    { value: '🚚 rename ', name: '🚚 rename:\t파일명, 폴더명 수정 또는 이동' },
-    { value: '🔥 remove ', name: '🔥 remove:\t파일, 폴더 삭제' },
-    { value: '📝 docs ', name: '📝 docs:\t문서가 추가되거나 변경된 경우 (i18n 포함)' },
-    { value: '👷 chore ', name: '👷 chore:\t빌드 업무, 패키지 매니저 수정 및 추가'},
+    { value: 'feat', name: `${chalk.green(padLabel('[feat]'))} Add a new feature` },
+    { value: 'fix', name: `${chalk.red(padLabel('[fix]'))} Fix a bug or issue` },
+    { value: 'design', name: `${chalk.cyan(padLabel('[design]'))} Update UI design` },
+    { value: 'style', name: `${chalk.yellow(padLabel('[style]'))} Code style changes (no logic changes)` },
+    { value: 'refactor', name: `${chalk.magenta(padLabel('[refactor]'))} Refactor code (no feature changes)` },
+    { value: 'test', name: `${chalk.blue(padLabel('[test]'))} Add or update tests` },
+    { value: 'rename', name: `${chalk.gray(padLabel('[rename]'))} Rename a file or folder` },
+    { value: 'move', name: `${chalk.gray(padLabel('[move]'))} Move a file or folder` },
+    { value: 'remove', name: `${chalk.gray(padLabel('[remove]'))} Delete a file or folder` },
+    { value: 'docs', name: `${chalk.white(padLabel('[docs]'))} Update documentation or i18n files` },
+    { value: 'chore', name: `${chalk.white(padLabel('[chore]'))} Build system or dependency changes` },
   ],
   allowCustomScopes: false,
   skipQuestions: ['body'],
   subjectLimit: 100,
-}
+};
